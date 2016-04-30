056a16340fabe41e19f509e4b22cef5153ddaf75
Isidora Djurdjevic <isidoradjurdjevic.100@gmail.com>
2016-04-30 18:04:43 +0200

    Added shape for enemy and endtile classes

--- enemy.h
+++ enemy.h
@@ -8,10 +8,13 @@ class Enemy : public QGraphicsItem
 {
 public:
     Enemy(qreal x, qreal y, QVector<QPointF *> path, int dim = TILE_DIM);
+    ~Enemy();
+
 
     QRectF boundingRect() const Q_DECL_OVERRIDE;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) Q_DECL_OVERRIDE;
-    QPointF getCenter();
+    QPointF getCenter() const;
+    QPainterPath shape() const Q_DECL_OVERRIDE;
 
 protected slots:
     void advance(int phase) Q_DECL_OVERRIDE;
